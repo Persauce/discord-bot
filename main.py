7979f204a957044af5298182bd65a7e8a737b15e
Dave Persaud <84549671+Persauce@users.noreply.github.com>
2023-12-28 10:31:21 -0500

    revamped

--- main.py
+++ main.py
@@ -1,25 +1,79 @@
+# Importing libraries
+import asyncio
+import os
+import time
+
 import discord
-from discord.ext import commands
+import youtube_dl
 
 import settings
 
+# Discord bot Initialization
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents = intents)
+key = settings.DISC_CODE
 
-def run():
-    
-    ints = discord.Intents.default()
-    ints.message_content = True
-    bot = commands.Bot(command_prefix="!",intents=ints)
-    @bot.event
-    async def on_ready():
-        print("logged in")
-         
-    @bot.command()
-    async def ping(ctx):
-        await ctx.send("pong")
-    
+voice_clients = {}
+
+yt_dl_opts = {'format': 'bestaudio/best'}
+ytdl = youtube_dl.YoutubeDL(yt_dl_opts)
+
+ffmpeg_options = {'options': "-vn"}
 
 
+# This event happens when the bot gets run
+@client.event
+async def on_ready():
+    print(f"Bot logged in as {client.user}")
 
-    bot.run(settings.DISC_CODE)
-if __name__ == "__main__":
-    run()
+
+# This event happens when a message gets sent
+@client.event
+async def on_message(msg):
+    if msg.content.startswith("?play"):
+
+        try:
+            voice_client = await msg.author.voice.channel.connect()
+            voice_clients[voice_client.guild.id] = voice_client
+        except:
+            print("error")
+
+        try:
+            url = msg.content.split()[1]
+
+            loop = asyncio.get_event_loop()
+            data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=True))
+
+            song = data['url']
+            player = discord.FFmpegPCMAudio(song,**ffmpeg_options,executable = "C:\\Users\\davep\\Desktop\\bin\\ffmpeg.exe")
+
+            voice_clients[msg.guild.id].play(player)
+
+        except Exception as err:
+            print(err)
+
+
+    if msg.content.startswith("?pause"):
+        try:
+            voice_clients[msg.guild.id].pause()
+        except Exception as err:
+            print(err)
+
+    # This resumes the current song playing if it's been paused
+    if msg.content.startswith("?resume"):
+        try:
+            voice_clients[msg.guild.id].resume()
+        except Exception as err:
+            print(err)
+
+    # This stops the current playing song
+    if msg.content.startswith("?stop"):
+        try:
+            voice_clients[msg.guild.id].stop()
+            await voice_clients[msg.guild.id].disconnect()
+        except Exception as err:
+            print(err)
+
+
+client.run(key)
